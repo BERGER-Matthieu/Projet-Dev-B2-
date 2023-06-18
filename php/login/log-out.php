4c363050983feeb23aee7aa84bd960f06a7e02d6
Matthieu Berger <104015028+BERGER-Matthieu@users.noreply.github.com>
2023-06-18 19:05:49 +0200

    remove friend + file reorganization

--- php/login/log-out.php
+++ php/login/log-out.php
@@ -2,6 +2,6 @@
     session_start();
     session_destroy();
     
-    $msg = "You've been sucsefully loged out";
+    $msg = "You've been successfully loged out";
     header("Location: http://localhost/Projet-Dev-B2-/pages/login-register.php?error=".$msg);
 ?>
